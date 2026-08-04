598a5e6c2e1157ec77ac4a23b52a2ff6a3233705
Code-Dawg <whatsupcodedwag@gmail.com>
2026-08-04 20:00:36 +0545

    dff

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django.urls import reverse
+
+
+@override_settings(ALLOWED_HOSTS=["testserver"])
+class HomePageTests(TestCase):
+    def test_home_page_renders(self):
+        response = self.client.get(reverse("home"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "EduPulse")
 
 # Create your tests here.
