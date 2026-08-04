f7df1f8d166cd8acd3a2dbc44289b82f08c394f9
Salfet-spec <sigdel.salfet321@gmail.com>
2026-08-04 22:41:18 +0545

    bb

--- core/urls.py
+++ core/urls.py
@@ -1,25 +1,30 @@
 """
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+==============================================================================
+ROOT URL ROUTER
+Delegates requests to specific modular Django app routers.
+==============================================================================
 """
-from django.contrib import admin
-from django.urls import path
 
-from home.views import home
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home, name='home'),
     path('admin/', admin.site.urls),
+    
+    # Feature App Routers
+    path('', include('home.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('footprints/', include('digitalfootprints.urls')),
+    path('journey/', include('journey.urls')),
+    path('quiz/', include('quiz.urls')),
+    path('leaderboard/', include('leaderboard.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('statistics/', include('statistics.urls')),
+    path('blog/', include('blog.urls')),
+    path('contact/', include('contact.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
